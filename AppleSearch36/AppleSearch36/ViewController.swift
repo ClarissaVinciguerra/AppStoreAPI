b7b2449e6d667659378af693952db14f15986c95
Clarissa Vinciguerra <clarissavinciguerra@Luiss-MacBook-Pro.local>
2020-09-24 09:18:55 -0700

    initial commit

---
+++ AppleSearch36/AppleSearch36/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleSearch36
+//
+//  Created by Clarissa Vinciguerra on 9/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
